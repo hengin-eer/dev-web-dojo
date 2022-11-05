cc375fb50a7183e9c3e020cb1e2c6f8806f68c28
hengin-eer <hirokix0608@gmail.com>
2022-11-06 02:38:42 +0900

    🎉

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   }
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className='App'>
         <Header active={active} setActive={setActive} />
 
