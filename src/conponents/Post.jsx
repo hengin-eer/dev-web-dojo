8c744456c9a91c2910c019381100cad871851c04
hengin-eer <hirokix0608@gmail.com>
2022-12-12 10:15:46 +0900

    update!

--- src/conponents/Post.jsx
+++ src/conponents/Post.jsx
@@ -4,15 +4,18 @@ import postlist from '../posts.json'
 import ReactMarkdown from 'react-markdown'
 import rehypeRaw from 'rehype-raw'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHashtag, faClockRotateLeft } from '@fortawesome/free-solid-svg-icons'
+import { faHashtag, faClockRotateLeft, faList } from '@fortawesome/free-solid-svg-icons'
 import { NotFound } from './NotFound'
+import { useState } from 'react'
+import Highlighter from 'react-syntax-highlighter'
+import { monokai } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 const Post = () => {
+    const [toclists, toggleToclists] = useState(false)
     const { id } = useParams()
     let fetchedPost = {
         title: ""
     }
-    console.log(id)
     postlist.forEach(post => {
         if (id === post.slug) {
             fetchedPost.title = post.title ? post.title : "No title"
@@ -20,7 +23,6 @@ const Post = () => {
             fetchedPost.date = post.date ? post.date : "No date"
             fetchedPost.category = post.category ? post.category : "No category"
             fetchedPost.content = post.content ? post.content : "No content"
-            console.log("success!")
         }
     })
     if (fetchedPost.title === "") {
@@ -29,11 +31,21 @@ const Post = () => {
         )
     }
 
-    const Toclist = ({ ...props }) => {
+    const Toclist1 = ({ ...props }) => {
         return (
             <li>
-                <a href={'#' + props.children.toString()}>{props.children.toString()}</a>
+                <a href={'#' + props.children.toString()} onClick={() => toggleToclists(false)}>{props.children.toString()}</a>
             </li>
+        )
+    }
+
+    const Toclist2 = ({ ...props }) => {
+        return (
+            <ul>
+                <li>
+                    <a href={'#' + props.children.toString()} onClick={() => toggleToclists(false)}>{props.children.toString()}</a>
+                </li>
+            </ul>
         )
     }
 
@@ -48,26 +60,56 @@ const Post = () => {
         )
     }
 
-    return (
-        <div className='post'>
-            <div className="post__back">
-                <Link to={`/posts/${fetchedPost.category}`}>記事一覧に戻る</Link>
-            </div>
-            <div className="post__date"><FontAwesomeIcon className='date__ico' icon={faClockRotateLeft} />{fetchedPost.date}</div>
-            <div className="post__ttl">{fetchedPost.title}</div>
-            <div className="post__cat"><FontAwesomeIcon className='cat__ico' icon={faHashtag} />{fetchedPost.category}</div>
-            <div className="post__author">
-                <img className='author__ico' src={`${process.env.PUBLIC_URL}/media/${fetchedPost.author}.png`} alt="" />
-                <div className="author__name">{fetchedPost.author}</div>
-            </div>
-            <div className="post__contents">
-                <div className="toc__ttl">INDEX</div>
-                <div className="toc">
-                    <ReactMarkdown components={{ h1: Toclist, h2: Toclist }} allowedElements={["h1", "h2"]} rehypePlugins={[rehypeRaw]} >{fetchedPost.content}</ReactMarkdown>
+    const CopyCode = ({ ...props }) => {
+        const [btnText, setBtnText] = useState('Copy')
+
+        const codeContents = props.children[0].props
+        const filename = codeContents.className.split(":")[1]
+        const copyCode = (codes) => {
+            navigator.clipboard.writeText(codes)
+            setBtnText('Copied!')
+            setTimeout(() => {
+                setBtnText('Copy')
+            }, 750);
+        }
+        return (
+            <div className='codes__wrapper'>
+                <div className="codes__header">
+                    <div className="file__name">{filename}</div>
+                    <button className='copy__codes' onClick={() => copyCode(codeContents.children.toString())}>{btnText}</button>
                 </div>
-                <ReactMarkdown components={{ h1: H1, h2: H2 }} rehypePlugins={[rehypeRaw]} >{fetchedPost.content}</ReactMarkdown>
+                <Highlighter style={monokai}>
+                    {codeContents.children}
+                </Highlighter>
             </div>
-            <hr />
+        )
+    }
+
+    return (
+        <div className='post' id='postTop'>
+            <div className={toclists ? "post__toc active" : "post__toc"}>
+                <div className="toc">
+                    <ReactMarkdown components={{ h1: Toclist1, h2: Toclist2 }} allowedElements={["h1", "h2"]} rehypePlugins={[rehypeRaw]} >{fetchedPost.content}</ReactMarkdown>
+                </div>
+            </div>
+            <div className={toclists ? "toggle__toclists active" : "toggle__toclists"} onClick={() => toggleToclists(!toclists)}>
+                <FontAwesomeIcon className='toclists__ico' icon={faList} />
+            </div>
+            <div className="post__article" onClick={() => toggleToclists(false)}>
+                <div className="post__back">
+                    <Link to={`/posts/${fetchedPost.category}`}>記事一覧に戻る</Link>
+                </div>
+                <div className="post__date"><FontAwesomeIcon className='date__ico' icon={faClockRotateLeft} />{fetchedPost.date}</div>
+                <div className="post__ttl">{fetchedPost.title}</div>
+                <div className="post__cat"><FontAwesomeIcon className='cat__ico' icon={faHashtag} />{fetchedPost.category}</div>
+                <div className="post__author">
+                    <img className='author__ico' src={`${process.env.PUBLIC_URL}/media/${fetchedPost.author}.png`} alt="" />
+                    <div className="author__name">{fetchedPost.author}</div>
+                </div>
+                <div className="post__contents">
+                    <ReactMarkdown components={{ h1: H1, h2: H2, pre: CopyCode }} rehypePlugins={[rehypeRaw]} >{fetchedPost.content}</ReactMarkdown>
+                </div>
+            </div>
         </div>
     )
 }
