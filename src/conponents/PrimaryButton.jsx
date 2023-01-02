d0f6b11f384b3aafa02a1abd5ffdf93437596f9c
hengin-eer <hirokix0608@gmail.com>
2023-01-02 19:53:19 +0900

    🐛 Fixed a bug that prevented transition to id

--- src/conponents/PrimaryButton.jsx
+++ src/conponents/PrimaryButton.jsx
@@ -2,11 +2,21 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { ReactComponent as BtnArrow } from '../media/btnArrow.svg'
 
-export const PrimaryButton = ({inner, to, className}) => {
-	return (
-		<Link to={to} className={`primary__button ${className}`}>
-			<div className='inner__txt'>{inner}</div>
-			<BtnArrow className='btn__arrow' />
-		</Link>
-	)
+export const PrimaryButton = ({ inner, to, className }) => {
+	let dontHaveSharp = to.indexOf('#') === -1
+	if (dontHaveSharp) {
+		return (
+			<Link to={to} className={`primary__button ${className}`}>
+				<div className='inner__txt'>{inner}</div>
+				<BtnArrow className='btn__arrow' />
+			</Link>
+		)
+	} else {
+		return (
+			<a href={to} className={`primary__button ${className}`}>
+				<div className='inner__txt'>{inner}</div>
+				<BtnArrow className='btn__arrow' />
+			</a>
+		)
+	}
 }
