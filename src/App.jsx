f1ac6b2bb5e1b33c18079cb2342722299e5f1f62
hengin-eer <hirokix0608@gmail.com>
2023-03-15 22:39:28 +0900

    pref&feat: #34 `React.lazy()`を用いた動的インポート、ローディングの実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
 import './styles/style.css'
-import Header from './conponents/Header';
-import HomePage from './conponents/HomePage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CoursePage from './conponents/CoursePage';
-import AboutPage from './conponents/AboutPage';
-import { useState } from 'react';
-import Postlist from './conponents/Postlist';
-import Post from './conponents/Post';
-import ScrollToTop from './conponents/ScrollToTop';
-import { NotFound } from './conponents/NotFound';
-import Footer from './conponents/Footer';
+import { lazy, Suspense, useEffect, useState } from 'react';
+import { TailSpin } from 'react-loading-icons';
+// const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+// const CoursePage = lazy(() => sleep(500).then(() => import('./conponents/CoursePage')));
+const Header = lazy(() => import('./conponents/Header'));
+const HomePage = lazy(() => import('./conponents/HomePage'));
+const CoursePage = lazy(() => import('./conponents/CoursePage'));
+const Postlist = lazy(() => import('./conponents/Postlist'));
+const Post = lazy(() => import('./conponents/Post'));
+const AboutPage = lazy(() => import('./conponents/AboutPage'));
+const ScrollToTop = lazy(() => import('./conponents/ScrollToTop'));
+const NotFound = lazy(() => import('./conponents/NotFound'));
+const Footer = lazy(() => import('./conponents/Footer'));
+
+const FallBack = (props) => {
+  const { style } = props
+
+  return (
+    <div className='loading-animation' style={{
+      ...style,
+      height: "100vh",
+      width: "100vw",
+      position: "fixed",
+      background: "#fff",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      animation: "fadeOut .5s linear",
+      zIndex: "9999",
+    }}>
+      <TailSpin height={60} width={60} stroke='#2869ff' speed={1} />
+    </div >
+  )
+}
 
 function App() {
   const [active, setActive] = useState(false);
@@ -19,24 +43,37 @@ function App() {
     document.body.classList.remove('active');
   }
 
+
+  const [show, setShow] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true)
+    }, 300)
+  }, [])
+
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <div className='App'>
-        <Header active={active} setActive={setActive} />
-        <ScrollToTop />
+    <Suspense fallback={<FallBack />}>
+      <Router basename={process.env.PUBLIC_URL}>
+        <div className='App'>
+          {<FallBack style={{ opacity: show ? 0 : 1, visibility: show ? "hidden" : "visible", }} />}
+          <Header active={active} setActive={setActive} />
+          <ScrollToTop />
 
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path='/courses/' element={<CoursePage />} />
-          <Route path='/about/' element={<AboutPage />} />
-          <Route path='/posts/:cat/' element={<Postlist />} />
-          <Route path='/posts/:cat/:id/' element={<Post />} />
-        </Routes>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path='/courses/' element={<CoursePage />} />
+            <Route path='/about/' element={<AboutPage />} />
+            <Route path='/posts/:cat/' element={<Postlist />} />
+            <Route path='/posts/:cat/:id/' element={<Post />} />
+          </Routes>
 
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+
+        </div>
+      </Router>
+    </Suspense>
   );
 }
 
