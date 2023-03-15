99f8f305261e4adde6a031887cbe94e95b4b2a67
hengin-eer <hirokix0608@gmail.com>
2023-03-15 19:15:24 +0900

    pref: #36 nitacwplロゴのリサイズによる修正

--- src/conponents/Header.jsx
+++ src/conponents/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-import nitacwpl from '../media/logo.png'
+import nitacwpl from '../media/logo-min.png'
 import { Link } from 'react-router-dom'
 import { ReactComponent as Logo } from '../media/logo.svg'
 
