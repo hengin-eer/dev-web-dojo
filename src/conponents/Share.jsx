46fe3dc8e94d50be30a5feec9a54a9131ddd5af1
hengin-eer <hirokix0608@gmail.com>
2023-03-25 21:33:17 +0900

    fix: #49 Facebook, Pinterestの復活とインスタ, Tiktokの削除

--- src/conponents/Share.jsx
+++ src/conponents/Share.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInstagram, faLine, faTiktok, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faFacebook, faLine, faPinterest, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { ReactComponent as HatenaBookmark } from '../media/hatenabookmark.svg'
 import React, { useState } from 'react'
 import { faCheck, faPaperclip } from '@fortawesome/free-solid-svg-icons'
@@ -11,10 +11,10 @@ const Share = () => {
     const url = `https://web-dojo.nitacwpl.tech${currentPath}`
     const text = encodeURIComponent(document.title);
     const twitterUrl = `https://twitter.com/intent/tweet?url=${url}&text=${text}`;
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${url}`;
     const lineUrl = `https://social-plugins.line.me/lineit/share?url=${url}`;
     const hatenaUrl = `http://b.hatena.ne.jp/add?mode=confirm&url=${url}`;
-    const instagramUrl = `https://www.instagram.com/create/broadcast/?source_url=${url}`
-    const tiktokUrl = `https://www.tiktok.com/upload/?refer=${url}`
+    const pinterestUrl = `https://www.pinterest.com/pin/create/button/?url=${url}`;
 
     const handleCopy = () => {
         navigator.clipboard.writeText(url)
@@ -34,17 +34,17 @@ const Share = () => {
                 <a className='share__links__btn' href={twitterUrl} target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faTwitter} color='#1da1f2' />
                 </a>
+                <a className='share__links__btn' href={facebookUrl} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faFacebook} color='#1877f2' />
+                </a>
                 <a className='share__links__btn' href={lineUrl} target="_blank" rel="noopener noreferrer">
                     <FontAwesomeIcon icon={faLine} height="50px" color='#00c300' />
                 </a>
                 <a className='share__links__btn' href={hatenaUrl} target="_blank" rel="noopener noreferrer">
                     <HatenaBookmark fill='#00A4DE' />
                 </a>
-                <a className='share__links__btn' href={instagramUrl} target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faInstagram} color='#E4405F' />
-                </a>
-                <a className='share__links__btn' href={tiktokUrl} target="_blank" rel="noopener noreferrer">
-                    <FontAwesomeIcon icon={faTiktok} color='#000000' />
+                <a className='share__links__btn' href={pinterestUrl} target="_blank" rel="noopener noreferrer">
+                    <FontAwesomeIcon icon={faPinterest} color='#e60023' />
                 </a>
             </div>
         </div>
